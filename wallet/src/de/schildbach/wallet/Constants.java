07af5d7f5736badb185eff5f3269f23ee97c9b40
Stouse49 <Stouse49@gmail.com>
2019-01-15 20:21:16 -0800

    Add constant to disable electrum support

--- wallet/src/de/schildbach/wallet/Constants.java
+++ wallet/src/de/schildbach/wallet/Constants.java
@@ -224,4 +224,6 @@ public final class Constants {
     /** Base URL for blockchain API. */
     public static final String BITEASY_API_URL = NETWORK_PARAMETERS.getId().equals(NetworkParameters.ID_MAINNET) ? BITEASY_API_URL_PROD
             : BITEASY_API_URL_TEST;
+
+    public static final boolean GOLDCOIN_ELECTRUM_SUPPORT = false;
 }
